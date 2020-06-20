d1eaecaa8a2a6c389dfd30eda60b697b930703c4
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-06-20 16:37:58 +0545

    Added question controller

---
+++ Server/Controllers/QuestionController.cs
@@ -0,0 +1,31 @@
+﻿using Kolpi.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Kolpi.Shared.ViewModels;
+
+namespace Kolpi.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class QuestionController : ControllerBase
+    {
+        private readonly ILogger<QuestionController> logger;
+
+        public QuestionController(ILogger<QuestionController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] QuestionViewModel questionViewModel)
+        {
+
+        }
+    }
+}
