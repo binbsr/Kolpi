9396935d0c79d1e50a652a08dfacb9ce8d9d2801
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-06-29 15:14:51 +0545

    Added logging infra

---
+++ Server/Infrastructure/Logging/IKolpiLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace Kolpi.Server.Infrastructure.Logging
+{
+    public interface IKolpiLogger<T>
+    {
+        void LogInformation(string message, params object[] args);
+        void LogWarning(string message, params object[] args);
+        void LogError(string message, params object[] args);
+    }
+}
