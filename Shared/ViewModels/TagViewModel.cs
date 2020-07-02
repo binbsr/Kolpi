4a8705b8f90ca13fa236a6ddaa2bfb40be8c3e2e
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-07-02 22:25:22 +0545

    Modified tagviewmodel and refactoring after effects

--- Shared/ViewModels/TagViewModel.cs
+++ Shared/ViewModels/TagViewModel.cs
@@ -1,9 +1,19 @@
+using System;
+
 namespace Kolpi.Shared.ViewModels
 {
     public class TagViewModel
     {
-        public string Label { get; set; }
-        public string Value { get; set; }
-        public string ColorCode { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public bool IsFinalized { get; set; }
+
+        public string TagTypeName { get; set; }
+        public string TagColorCode { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedAt { get; set; }
+        public string ModifiedBy { get; set; }
     }
 }
