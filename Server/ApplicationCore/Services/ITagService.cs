d83883f259b6df768a44aa1662af56fe92526c00
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-07-02 22:32:04 +0545

    Added tag service

---
+++ Server/ApplicationCore/Services/ITagService.cs
@@ -0,0 +1,13 @@
+﻿using Kolpi.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolpi.Server.ApplicationCore.Services
+{
+    public interface ITagService : IAsyncService<Tag, int>
+    {
+        Task<List<Tag>> GetAllAsync(int pageIndex, int pageSize);
+    }
+}
