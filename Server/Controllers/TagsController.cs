853daf841361da7761fd472c3824a55cecf4c494
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-07-02 22:32:26 +0545

    Added tag api controller and mapper

---
+++ Server/Controllers/TagsController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Kolpi.Shared.ViewModels;
+using Kolpi.Shared.Mapper;
+using System.Net;
+using Kolpi.Server.ApplicationCore.Services;
+using Kolpi.Shared.Models;
+
+namespace Kolpi.Server.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TagsController : ControllerBase
+    {
+        private readonly ITagService tagService;
+        private readonly ILogger<TagsController> logger;
+
+        public TagsController(ITagService tagService, ILogger<TagsController> logger)
+        {
+            this.tagService = tagService;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TagViewModel>>> Get([FromQuery] int pageIndex = 1, int pageSize = 10)
+        {
+            var tagModels = await tagService.GetAllAsync(pageIndex, pageSize);
+            var tagViewModels = tagModels.ToViewModel();
+            return Ok(tagViewModels);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TagViewModel>> Get([FromRoute] int id)
+        {
+            var tagTypeModel = await tagService.GetByIdAsync(id);
+
+            if (tagTypeModel == null)
+                return NotFound();
+
+            var tagTypeViewModel = tagTypeModel.ToViewModel();
+
+            return Ok(tagTypeViewModel);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] TagViewModel tagViewModel)
+        {
+            if (tagViewModel == null || string.IsNullOrWhiteSpace(tagViewModel.Name))
+                return BadRequest();
+
+            var tagModel = tagViewModel.ToModel();
+            var rowsAdded = await tagService.AddAsync(tagModel);
+            
+            if (rowsAdded <= 0)
+                return Problem("Could not insert into the store.", nameof(tagModel), (int)HttpStatusCode.InternalServerError, "Data Insert");
+
+            return CreatedAtAction(nameof(Get), new { id = tagModel.Id }, tagModel);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<int>> Put([FromBody] TagViewModel tagViewModel)
+        {
+            if (tagViewModel == null || tagViewModel.Id == default)
+                return BadRequest();
+
+            var tagModel = tagViewModel.ToModel();
+
+            var rowsUpdated = await tagService.UpdateAsync(tagModel);
+            if (rowsUpdated <= 0)
+                return Problem("Could not modify store.", nameof(Tag), (int)HttpStatusCode.InternalServerError, "Data Update");
+
+            return Ok(rowsUpdated);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            if (id == default)
+                return BadRequest();
+
+            var rowsDeleted = await tagService.DeleteAsync(id);
+
+            if (rowsDeleted <= 0)
+                return Problem("Could not delete from store.", nameof(Tag), (int)HttpStatusCode.InternalServerError, "Data Deletion");
+
+            return NoContent();
+        }
+    }
+}
---
+++ Shared/Mapper/TagMapper.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Kolpi.Shared.Models;
+using Kolpi.Shared.ViewModels;
+
+namespace Kolpi.Shared.Mapper
+{
+    public static class TagMapper
+    {
+        public static TagViewModel ToViewModel(this Tag model)
+        {
+            if (model == null)
+                return null;
+
+            var tagViewModel = new TagViewModel
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Details = model.Details,
+                IsFinalized = model.IsFinalized,
+                CreatedBy = model.CreatedBy,
+                CreatedAt = model.CreatedAt,
+                ModifiedBy = model.ModifiedBy,
+                ModifiedAt = model.ModifiedAt,
+                TagTypeName = model.TagType?.Name,
+                TagColorCode = model.TagType?.ColorCode
+            };
+
+            return tagViewModel;
+        }
+
+        public static Tag ToModel(this TagViewModel viewModel)
+        {
+            if (viewModel == null)
+                return null;
+
+            var tagModel = new Tag
+            {
+                Id = viewModel.Id,
+                Name = viewModel.Name,
+                Details = viewModel.Details,
+                IsFinalized = viewModel.IsFinalized,
+                CreatedBy = viewModel.CreatedBy,
+                CreatedAt = viewModel.CreatedAt,
+                ModifiedBy = viewModel.ModifiedBy,
+                ModifiedAt = viewModel.ModifiedAt
+            };
+
+            return tagModel;
+        }
+
+        public static List<TagViewModel> ToViewModel(this IEnumerable<Tag> tagModels)
+        {
+            List<TagViewModel> tagViewModels = new List<TagViewModel>();
+            foreach (var model in tagModels)
+            {
+                tagViewModels.Add(model.ToViewModel());
+            }
+
+            return tagViewModels;
+        }
+
+        public static List<Tag> ToModel(this IEnumerable<TagViewModel> tagViewModels)
+        {
+            List<Tag> tagModels = new List<Tag>();
+            foreach (var model in tagViewModels)
+            {
+                tagModels.Add(model.ToModel());
+            }
+
+            return tagModels;
+        }
+    }
+}
