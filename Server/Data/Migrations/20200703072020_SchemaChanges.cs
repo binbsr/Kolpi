4f69cbc230c9ca8bd11752b5b13573a9b577b12e
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-07-03 15:15:12 +0545

    Added migration for editbase column names and relationships

---
+++ Server/Data/Migrations/20200703072020_SchemaChanges.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kolpi.Server.Data.Migrations
+{
+    public partial class SchemaChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_QuestionTypes_QuestionTypeId",
+                table: "Questions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_Questions_QuestionId",
+                table: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "QuestionTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_QuestionTypeId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "QuestionTypeId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuestionId",
+                table: "Tags",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Tags",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Tags",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedAt",
+                table: "Tags",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModifiedBy",
+                table: "Tags",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionStatusId",
+                table: "Questions",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "QuestionTags",
+                columns: table => new
+                {
+                    QuestionId = table.Column<int>(nullable: false),
+                    TagId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionTags", x => new { x.QuestionId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_QuestionTags_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QuestionTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_QuestionStatusId",
+                table: "Questions",
+                column: "QuestionStatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionTags_TagId",
+                table: "QuestionTags",
+                column: "TagId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_QuestionStatuses_QuestionStatusId",
+                table: "Questions",
+                column: "QuestionStatusId",
+                principalTable: "QuestionStatuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_Questions_QuestionId",
+                table: "Tags",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_QuestionStatuses_QuestionStatusId",
+                table: "Questions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tags_Questions_QuestionId",
+                table: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "QuestionTags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_QuestionStatusId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedAt",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "QuestionStatusId",
+                table: "Questions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuestionId",
+                table: "Tags",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionTypeId",
+                table: "Questions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "QuestionTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_QuestionTypeId",
+                table: "Questions",
+                column: "QuestionTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_QuestionTypes_QuestionTypeId",
+                table: "Questions",
+                column: "QuestionTypeId",
+                principalTable: "QuestionTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tags_Questions_QuestionId",
+                table: "Tags",
+                column: "QuestionId",
+                principalTable: "Questions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
