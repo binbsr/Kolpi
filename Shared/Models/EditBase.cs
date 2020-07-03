e8d38de9b2389146090e9b36f9c87c8b99596a5c
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-07-04 00:55:18 +0545

    Added editbase created and modified stamp methods

--- Shared/Models/EditBase.cs
+++ Shared/Models/EditBase.cs
@@ -4,9 +4,23 @@ namespace Kolpi.Shared.Models
 {
     public class EditBase<TKey> : BaseEntity<TKey>
     {
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
         public string CreatedBy { get; set; }
-        public DateTime ModifiedAt { get; set; }
+        public DateTime ModifiedAt { get; set; } = DateTime.Now;
         public string ModifiedBy { get; set; }
+
+        public EditBase<TKey> AddCreatedStamps(string createdBy)
+        {
+            CreatedBy = createdBy;
+            CreatedAt = DateTime.Now;
+            return this;
+        }
+
+        public EditBase<TKey> AddModifiedStamps(string modifiedBy)
+        {
+            ModifiedBy = modifiedBy;
+            ModifiedAt = DateTime.Now;
+            return this;
+        }
     }
 }
