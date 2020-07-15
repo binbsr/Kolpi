7f9a308caf8010de5473235f6000f7af1f8ae8f7
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-07-15 23:05:43 +0545

    Staged funtional test and fetch tagtypes tests

---
+++ Tests/Kolpi.FunctionalTests/Api/ApiTagtypesList.cs
@@ -0,0 +1,54 @@
+﻿using Kolpi.Server;
+using Kolpi.Shared.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kolpi.FunctionalTests.Api
+{
+    public class ApiTagtypesList : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient client;
+
+        public ApiTagtypesList(CustomWebApplicationFactory<Startup> applicationFactory)
+        {
+            client = applicationFactory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddAuthentication("Test")
+                        .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                            "Test", options => { });
+                });
+            })
+            .CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false,
+            });
+
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Test");
+        }
+
+        [Fact]
+        public async Task ReturnTagtypes()
+        {
+            var response = await client.GetAsync("/api/tagtypes");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonSerializer.Deserialize<IEnumerable<TagType>>(stringResponse);
+
+            Assert.Equal(2, result.Count());
+        }
+    }
+}
---
+++ Tests/Kolpi.FunctionalTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,68 @@
+﻿using Kolpi.Server;
+using Kolpi.Server.Data;
+using Kolpi.Server.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kolpi.FunctionalTests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder
+                .UseSolutionRelativeContentRoot("Src/Server")
+                .ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(
+                        d => d.ServiceType == typeof(DbContextOptions<KolpiDbContext>));
+
+                    if (descriptor != null)
+                    {
+                        services.Remove(descriptor);
+                    }
+
+                    // Add in memory dbcontext for testing
+                    services.AddDbContext<KolpiDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    });
+
+                    services.AddKolpiServices().AddKolpiLogger().AddKolpiEmailSender();
+
+                    // Build the service provider
+                    var sp = services.BuildServiceProvider();
+
+                    // Create a scope to obtain a reference to the database context
+
+                    using (var scope = sp.CreateScope())
+                    {
+                        var scopedServices = scope.ServiceProvider;
+                        var db = scopedServices.GetRequiredService<KolpiDbContext>();
+                        var logger = scopedServices
+                            .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                        db.Database.EnsureCreated();
+
+                        try
+                        {
+                            SeedData.PopulateTestData(db);
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.LogError(ex, "An error occurred seeding the " +
+                            $"database with test messages. Error: {ex.Message}");
+                        }
+                    }
+                });
+        }
+    }
+}
