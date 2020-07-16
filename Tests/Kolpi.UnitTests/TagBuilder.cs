3c9ce84e6a55c43eb0076bc6081c32a65a1a257c
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-07-16 10:35:34 +0545

    Using builder pattern for unit test data generation

---
+++ Tests/Kolpi.UnitTests/TagBuilder.cs
@@ -0,0 +1,41 @@
+﻿using Kolpi.Shared.Models;
+
+namespace Kolpi.UnitTests
+{
+    public class TagBuilder
+    {
+        private Tag tag = new Tag();
+
+        public TagBuilder Id(int id)
+        {
+            tag.Id = id;
+            return this;
+        }
+
+        public TagBuilder Name(string name)
+        {
+            tag.Name = name;
+            return this;
+        }
+
+        public TagBuilder Details(string details)
+        {
+            tag.Details = details;
+            return this;
+        }
+
+        public TagBuilder TagType(TagType tagType)
+        {
+            tag.TagType = tagType;
+            return this;
+        }
+
+        public TagBuilder WithDefaultValues()
+        {
+            tag = new Tag { Id = 1, Name = "Level-1", Details = "Simplest objective questions.", TagType = new TagTypeBuilder().WithDefaultValues().Build() };
+            return this;
+        }
+
+        public Tag Build() => tag;
+    }
+}
---
+++ Tests/Kolpi.UnitTests/TagTypeBuilder.cs
@@ -0,0 +1,41 @@
+﻿using Kolpi.Shared.Models;
+
+namespace Kolpi.UnitTests
+{
+    public class TagTypeBuilder
+    {
+        private TagType tagType = new TagType();
+
+        public TagTypeBuilder Id(int id)
+        {
+            tagType.Id = id;
+            return this;
+        }
+
+        public TagTypeBuilder Name(string name)
+        {
+            tagType.Name = name;
+            return this;
+        }
+
+        public TagTypeBuilder Details(string details)
+        {
+            tagType.Details = details;
+            return this;
+        }
+
+        public TagTypeBuilder ColorCode(string color)
+        {
+            tagType.ColorCode = color;
+            return this;
+        }
+
+        public TagTypeBuilder WithDefaultValues()
+        {
+            tagType = new TagType { Id = 1, Name = "Complexity", Details = "Defines complexity levels of questions.", ColorCode = "orange" };
+            return this;
+        }
+
+        public TagType Build() => tagType;
+    }
+}
