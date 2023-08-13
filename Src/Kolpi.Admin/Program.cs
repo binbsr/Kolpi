ea2e642da23e1a124017d64028f32436676be64b
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-08-13 22:10:34 +0545

    Start replacing mud with radzen

--- Src/Kolpi.Admin/Program.cs
+++ Src/Kolpi.Admin/Program.cs
@@ -1,7 +1,7 @@
 using Kolpi.Admin;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(client => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddMudServices();
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
 
 await builder.Build().RunAsync();
