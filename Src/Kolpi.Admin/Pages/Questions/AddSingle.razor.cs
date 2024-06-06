3f056a1e6b13843a3da1d50a8e0123262184836a
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-06-06 09:53:31 +0545

    Chnages on questions ui

--- Src/Kolpi.Admin/Pages/Questions/AddSingle.razor.cs
+++ Src/Kolpi.Admin/Pages/Questions/AddSingle.razor.cs
@@ -12,92 +12,56 @@ public partial class AddSingle
 
     [Inject]
     public NotificationService Notification { get; set; }
-
-    private QuestionViewModel Question = new();
-    List<AnswerOptionViewModel> answerOptionViewModels = new() { new(), new(), new(), new() };
-    private List<TagViewModel> Tags { get; set; } = default!;
-    List<TagViewModel> tagsSelected = new();
+    
     bool isSaving = false;
-
-    protected override async Task OnInitializedAsync()
-    {
-        var result = await Http.GetFromJsonAsync<TagsMetaViewModel>($"api/tags?take=999999") ?? new TagsMetaViewModel();
-        Tags = result.Records;
-    }
-
-    public void AddNewOption()
-    {
-        answerOptionViewModels.Add(new());
-    }
-
-    public void DeleteAnsOption(AnswerOptionViewModel answerOptionViewModel)
-    {
-        answerOptionViewModels.Remove(answerOptionViewModel);
-    }
+    private QuestionViewModel Question = new();
 
     public async Task OnSave()
     {
         isSaving = true;
 
-        Task<HttpResponseMessage> saveTask;
+        //Task<HttpResponseMessage> saveTask;
 
-        Question.AnswerOptions = answerOptionViewModels;
-        Question.Tags = tagsSelected;
+        //Question.AnswerOptions = answerOptionViewModels;
+        //Question.Tags = tagsSelected;
 
-        if (Question.Id == default)
-        {
-            // Adding new
-            saveTask = Http.PostAsJsonAsync("api/questions", Question);
-        }
-        else
-        {
-            // Modify existing
-            saveTask = Http.PutAsJsonAsync($"api/questions/{Question.Id}", Question);
-        }
+        //if (Question.Id == default)
+        //{
+        //    // Adding new
+        //    saveTask = Http.PostAsJsonAsync("api/questions", Question);
+        //}
+        //else
+        //{
+        //    // Modify existing
+        //    saveTask = Http.PutAsJsonAsync($"api/questions/{Question.Id}", Question);
+        //}
 
-        await saveTask.ContinueWith(st =>
-        {
-            isSaving = false;
+        //await saveTask.ContinueWith(st =>
+        //{
+        //    isSaving = false;
 
-            var result = st.Result;
+        //    var result = st.Result;
 
-            if (result.IsSuccessStatusCode)
-            {
-                Notification.Notify(
-                   new NotificationMessage
-                   {
-                       Summary = $"Question saved successfully",
-                       Severity = NotificationSeverity.Success,
-                       Duration = 4000
-                   });
-            }
-            else
-            {
-                Notification.Notify(
-                  new NotificationMessage
-                  {
-                      Summary = $"Error occured while saving question. Problem: {result.ReasonPhrase}",
-                      Severity = NotificationSeverity.Error,
-                      Duration = 4000
-                  });
-            }
-        });
-    }
-
-
-    public async Task<IEnumerable<TagViewModel>> Search(string value)
-    {
-        List<TagViewModel> emptyModel = new();
-
-        if (string.IsNullOrWhiteSpace(value))
-            return emptyModel;
-
-        Tags = await Http.GetFromJsonAsync<List<TagViewModel>>($"api/tags?searchText={value}") ?? emptyModel;
-        return Tags;
-    }
-
-    public void OnInput(string body)
-    {
-
+        //    if (result.IsSuccessStatusCode)
+        //    {
+        //        Notification.Notify(
+        //           new NotificationMessage
+        //           {
+        //               Summary = $"Question saved successfully",
+        //               Severity = NotificationSeverity.Success,
+        //               Duration = 4000
+        //           });
+        //    }
+        //    else
+        //    {
+        //        Notification.Notify(
+        //          new NotificationMessage
+        //          {
+        //              Summary = $"Error occured while saving question. Problem: {result.ReasonPhrase}",
+        //              Severity = NotificationSeverity.Error,
+        //              Duration = 4000
+        //          });
+        //    }
+        //});
     }
 }
