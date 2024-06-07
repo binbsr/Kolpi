b99e98784f851b7f6a258ca03dac1b2c9a92590b
AnupamaChaudhary1 <anupamachaudhary183@gmail.com>
2024-06-07 14:10:20 +0545

    error corrected

--- Src/Kolpi.Admin/Pages/Questions/AddSingle.razor.cs
+++ Src/Kolpi.Admin/Pages/Questions/AddSingle.razor.cs
@@ -1,3 +1,4 @@
+
 ﻿using Kolpi.WebShared.ViewModels;
 using Microsoft.AspNetCore.Components;
 using Radzen;
@@ -9,7 +10,7 @@ using Kolpi.ApplicationCore.Enums;  //  to resolve QuestionType reference
 namespace Kolpi.Admin.Pages.Questions
 {
 
-    public partial class AddSingle: AddSingleBase
+    public partial class AddSingle : ComponentBase
     {
         [Inject]
         public HttpClient Http { get; set; }
@@ -19,8 +20,8 @@ namespace Kolpi.Admin.Pages.Questions
 
         private List<QuestionViewModel> questions = new List<QuestionViewModel>();
         private List<TagViewModel> Tags { get; set; } = new List<TagViewModel>();
-        private bool isSavingQuestions;
-        
+        private bool isSaving;
+
 
         protected override async Task OnInitializedAsync()
         {
@@ -48,9 +49,9 @@ namespace Kolpi.Admin.Pages.Questions
             questions.Remove(question);
         }
 
-        private async Task OnSaveQuestions()
+        private async Task OnSave()
         {
-            isSavingQuestions = true;
+            isSaving = true;
 
             foreach (var question in questions)
             {
@@ -79,7 +80,7 @@ namespace Kolpi.Admin.Pages.Questions
                 }
             }
 
-            isSavingQuestions = false;
+            isSaving = false;
         }
 
         private List<TagViewModel> tagsSelected = new();
@@ -104,168 +105,26 @@ namespace Kolpi.Admin.Pages.Questions
         }
     }
 
+   
+
     public class AddComponentBase
     {
 
-    [Inject]
-    public HttpClient Http { get; set; }
+        [Inject]
+        public HttpClient Http { get; set; }
 
-    [Inject]
-    public NotificationService Notification { get; set; }
-    
-    bool isSaving = false;
-    private QuestionViewModel Question = new();
+        [Inject]
+        public NotificationService Notification { get; set; }
 
-    public async Task OnSave()
+        bool isSaving = false;
+        private QuestionViewModel Question = new();
+    }
+    public abstract class AddSingleBase : ComponentBase
     {
-        isSaving = true;
+        [Inject]
+        protected HttpClient Http { get; set; } = default!;
 
-        //Task<HttpResponseMessage> saveTask;
-
-        //Question.AnswerOptions = answerOptionViewModels;
-        //Question.Tags = tagsSelected;
-
-        //if (Question.Id == default)
-        //{
-        //    // Adding new
-        //    saveTask = Http.PostAsJsonAsync("api/questions", Question);
-        //}
-        //else
-        //{
-        //    // Modify existing
-        //    saveTask = Http.PutAsJsonAsync($"api/questions/{Question.Id}", Question);
-        //}
-
-        //await saveTask.ContinueWith(st =>
-        //{
-        //    isSaving = false;
-
-        //    var result = st.Result;
-
-        //    if (result.IsSuccessStatusCode)
-        //    {
-        //        Notification.Notify(
-        //           new NotificationMessage
-        //           {
-        //               Summary = $"Question saved successfully",
-        //               Severity = NotificationSeverity.Success,
-        //               Duration = 4000
-        //           });
-        //    }
-        //    else
-        //    {
-        //        Notification.Notify(
-        //          new NotificationMessage
-        //          {
-        //              Summary = $"Error occured while saving question. Problem: {result.ReasonPhrase}",
-        //              Severity = NotificationSeverity.Error,
-        //              Duration = 4000
-        //          });
-        //    }
-        //});
-
-
-//using Kolpi.WebShared.ViewModels;
-//using Microsoft.AspNetCore.Components;
-//using Radzen;
-//using System.Net.Http.Json;
-//using System.Threading.Tasks;
-//using System.Collections.Generic;
-
-//namespace Kolpi.Admin.Pages.Questions;
-
-//public partial class AddSingle
-//{
-//    [Inject]
-//    public HttpClient Http { get; set; }
-
-//    [Inject]
-//    public NotificationService Notification { get; set; }
-
-//    private QuestionViewModel Question = new();
-//    List<AnswerOptionViewModel> answerOptionViewModels = new() { new(), new(), new(), new() };
-//    private List<TagViewModel> Tags { get; set; } = default!;
-//    List<TagViewModel> tagsSelected = new();
-//    bool isSaving = false;
-
-//    protected override async Task OnInitializedAsync()
-//    {
-//        var result = await Http.GetFromJsonAsync<TagsMetaViewModel>($"api/tags?take=999999") ?? new TagsMetaViewModel();
-//        Tags = result.Records;
-//    }
-
-//    public void AddNewOption()
-//    {
-//        answerOptionViewModels.Add(new());
-//    }
-
-//    public void DeleteAnsOption(AnswerOptionViewModel answerOptionViewModel)
-//    {
-//        answerOptionViewModels.Remove(answerOptionViewModel);
-//    }
-//    public async Task OnSave()
-//    {
-//        isSaving = true;
-
-//        Task<HttpResponseMessage> saveTask;
-
-//        Question.AnswerOptions = answerOptionViewModels;
-//        Question.Tags = tagsSelected;
-
-//        if (Question.Id == default)
-//        {
-//            // Adding new
-//            saveTask = Http.PostAsJsonAsync("api/questions", Question);
-//        }
-//        else
-//        {
-//            // Modify existing
-//            saveTask = Http.PutAsJsonAsync($"api/questions/{Question.Id}", Question);
-//        }
-
-//        await saveTask.ContinueWith(st =>
-//        {
-//            isSaving = false;
-
-//            var result = st.Result;
-
-//            if (result.IsSuccessStatusCode)
-//            {
-//                Notification.Notify(
-//                   new NotificationMessage
-//                   {
-//                       Summary = $"Question saved successfully",
-//                       Severity = NotificationSeverity.Success,
-//                       Duration = 4000
-//                   });
-//            }
-//            else
-//            {
-//                Notification.Notify(
-//                  new NotificationMessage
-//                  {
-//                      Summary = $"Error occured while saving question. Problem: {result.ReasonPhrase}",
-//                      Severity = NotificationSeverity.Error,
-//                      Duration = 4000
-//                  });
-//            }
-//        });
-//    }
-
-
-//    public async Task<IEnumerable<TagViewModel>> Search(string value)
-//    {
-//        List<TagViewModel> emptyModel = new();
-
-//        if (string.IsNullOrWhiteSpace(value))
-//            return emptyModel;
-
-//        Tags = await Http.GetFromJsonAsync<List<TagViewModel>>($"api/tags?searchText={value}") ?? emptyModel;
-//        return Tags;
-//    }
-
-//    public void OnInput(string body)
-//    {
-
-//    }
-//}
+        [Inject]
+        protected NotificationService Notification { get; set; } = default!;
+    }
+}
