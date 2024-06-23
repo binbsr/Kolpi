954fc6fbd032080a2b53f679f474e8cb57755e65
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-06-23 14:30:51 +0545

    Remove old wasm templates and refactoring

--- Tests/Kolpi.FunctionalTests/Api/ApiTagtypesList.cs
+++ Tests/Kolpi.FunctionalTests/Api/ApiTagtypesList.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
